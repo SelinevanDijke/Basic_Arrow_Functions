cf3381b5c23913353982a4976a3e08e18ee5807c
Seline van Dijke <seline.vandijke98@gmail.com>
2021-09-27 20:43:33 +0200

    Greeting aangepast

---
+++ Basic_Arrow_Functions.js
@@ -0,0 +1,15 @@
+// Deel A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+
+// Deel B
+const fivePlusSeven = (nummer) => 5 + 7
+
+// Deel C
+const addAPlusB = (a, b) => a + b
+
+// Deel D
+const addFive = (a, addedNumber) => a + 5
+
+// Deel E
+const createObject = () => ({greeting: "Hallo!"})
+
